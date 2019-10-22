8d3a23864f7740364e0b589222f1a80ee32b4035
Kajo1737 <kajo1737@student.ju.se>
2019-10-22 09:01:13 +0200

    Initial Commit

---
+++ HelloSingleView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloSingleView
+//
+//  Created by Johan Kantola on 2019-10-22.
+//  Copyright © 2019 Johan Kantola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
